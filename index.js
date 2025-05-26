ddeccde868f81688700d5b1867abde4cca685d5a
Brendan Cantillon <brendancantillon@gmail.com>
2025-05-25 22:05:04 -0700

    defined trackerEmbed

--- index.js
+++ index.js
@@ -50,7 +50,11 @@ client.on('messageCreate', async message => {
     if (runIdMatch) runId = runIdMatch[1].replace(/[*_`~]/g, '').trim();
   }
 
-  
+  const trackerEmbed = new EmbedBuilder()
+  .setTitle('📥 Sign-Up Tracker')
+  .setDescription('Click your role to be logged in the signup sheet for this event. You can also undo.')
+  .setColor(0x00AE86);
+
 
   const row1 = new ActionRowBuilder().addComponents(
     new ButtonBuilder().setCustomId(`signup_tank_${message.id}`).setLabel('🛡 Tank').setStyle(ButtonStyle.Primary),
